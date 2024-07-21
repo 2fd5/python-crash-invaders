e66cf0132bc1ee8c45c5341e226dba04238d357e
Maciej Piatek <magic@2fd5.com>
2024-07-21 19:56:11 +0200

    Constant movement right

--- game_functions.py
+++ game_functions.py
@@ -14,7 +14,10 @@ def check_events(ship: Ship):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 # Move the ship to the right
-                ship.rect.centerx += 1
+                ship.moving_right = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
 
 def update_screen(ai_settings: Settings , screen: pygame.Surface, ship: Ship):
     """Update images on the screen and flip to the new screen"""
