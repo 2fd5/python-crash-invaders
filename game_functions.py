7b19db07b6b109e8ba9e162f1b4315d7493f047f
Maciej Piatek <magic@2fd5.com>
2024-07-21 21:03:11 +0200

    Breakdown key up and key down checks

--- game_functions.py
+++ game_functions.py
@@ -10,18 +10,25 @@ def check_events(ship: Ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        
+
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move the ship to the right
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+
+def check_keydown_events(event: pygame.event, ship: Ship):
+    """Respond to keypress."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event: pygame.event, ship: Ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 def update_screen(ai_settings: Settings , screen: pygame.Surface, ship: Ship):
     """Update images on the screen and flip to the new screen"""
