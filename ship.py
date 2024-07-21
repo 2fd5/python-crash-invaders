a4f3ea1f944500aad78999e04c7224d8df031a4c
Maciej Piatek <magic@2fd5.com>
2024-07-21 20:53:33 +0200

    Speed in settings

--- ship.py
+++ ship.py
@@ -1,10 +1,13 @@
 import pygame
 
+from settings import Settings
+
 class Ship():
 
-    def __init__(self, screen: pygame.Surface) -> None:
+    def __init__(self, ai_settings: Settings, screen: pygame.Surface) -> None:
         """Initialize the ship and set its starting position."""
         self.screen: pygame.surface = screen
+        self.ai_settings: Settings = ai_settings
 
         # Load the ship image and get its rect.
         self.image: pygame.Surface = pygame.image.load('images/icons/ffffff/transparent/1x1/lorc/deathcab.png').convert()
@@ -23,16 +26,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Store a decimal values for ship's center.
+        self.center = float(self.rect.centerx)
+
         # Movement flag
         self.moving_right: bool = False
         self.moving_left: bool = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
+        # Update the ship's center value, not the rect.
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
